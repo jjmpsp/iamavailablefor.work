c9ace3336df12de6daabbe24e3f8be503fe0f8c3
Joel Murphy <360hex@googlemail.com>
2015-08-21 01:19:12 +0100

    Progress bar added to account page

--- application/views/home.php
+++ application/views/home.php
@@ -33,16 +33,13 @@
 
                 // Show the progress bar 
                 NProgress.start();
-
                 // Increase randomly
                 var interval = setInterval(function() { NProgress.inc(); }, 1000);        
-
                 // Trigger finish when page fully loaded
                 jQuery(window).load(function () {
                     clearInterval(interval);
                     NProgress.done();
                 });
-
                 // Trigger bar when exiting the page
                 jQuery(window).unload(function () {
                     NProgress.start();
--- static/js/account.js
+++ static/js/account.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function(){
 
+    // Show the progress bar 
+    NProgress.start();
+    // Increase randomly
+    var interval = setInterval(function() { NProgress.inc(); }, 1000);        
+    // Trigger finish when page fully loaded
+    jQuery(window).load(function () {
+        clearInterval(interval);
+        NProgress.done();
+    });
+    // Trigger bar when exiting the page
+    jQuery(window).unload(function () {
+        NProgress.start();
+    });
+
     $('#myModal').on('show.uk.modal', function () {
         $('#myModal .notice').html("");
     });
